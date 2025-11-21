50f95dafe0facfab920de450f5c4de377bfde848
Marsel Galimullin <m.galimullin@tbank.ru>
2025-11-21 11:34:27 +0300

    one redis

--- sharding-repl-cache/scripts/redis-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-###
-# Инициализируем redis
-###
- docker exec -it redis_1 sh -c 'echo "yes" | redis-cli --cluster create 173.17.0.41:6379   173.17.0.42:6379   173.17.0.43:6379   173.17.0.44:6379   173.17.0.45:6379   173.17.0.46:6379 --cluster-replicas 1'
