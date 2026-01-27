d3613b76d8dcc78525874019d98029b4dbc0d41d
Baratashvili Vadim <bar_vad@list.ru>
2026-01-27 10:04:48 +0300

    Fix

--- scripts/init-shards-repl.sh
+++ scripts/init-shards-repl.sh
@@ -438,6 +438,24 @@ for shard in shard1 shard2 shard3; do
 done	
 
 echo ''
+echo '=== Init data ==='
+result=$(mongosh --host mongos1:27017 --eval "
+    db = db.getSiblingDB('somedb');	
+    db.createCollection('helloDoc');
+	sleep(10000);
+    print('✓ Created collection helloDoc\n'); 
+    sh.shardCollection('somedb.helloDoc', { age: 'hashed' });
+	sleep(10000);
+    print('✓ Sharded collection with hashed key on age field\n');  
+    for (let i = 0; i < 1000; i++) {
+        db.helloDoc.insertOne({age: i, name: 'ly' + i});
+    }
+")
+
+echo ''
+echo $result
+
+echo ''
 echo '✅ Initialization complete! Cluster is ready for use.'
 echo ''
 echo '🔌 Connection string for applications:'
