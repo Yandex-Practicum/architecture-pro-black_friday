1d398fa793ad168b0e59e55a9b70c4c718d26218
Andrei Fyodorov <afjord@ya.ru>
2026-02-22 12:26:16 +0300

    Task2: Add configSrv init (not tested)

--- mongo-sharding/mongo-init.sh
+++ mongo-sharding/mongo-init.sh
@@ -9,3 +9,4 @@ use somedb
 for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
 EOF
 
+docker exec configSrv mongosh --port 27017 --quiet --eval 'try { rs.status() } catch(e) { rs.initiate({_id:"config_server",configsvr:true,members:[{_id:0,host:"configSrv:27017"}]}) }'
